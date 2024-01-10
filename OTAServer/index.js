70e94b406b50f5492e61de3cd42640eb17749f18
Dodo765 <104673265+Dodo765@users.noreply.github.com>
2024-01-10 21:11:11 +0100

    d

--- OTAServer/index.js
+++ OTAServer/index.js
@@ -9,13 +9,11 @@ const nets = networkInterfaces();
 // Server port
 const PORT = 3000;
 
-let downloadCounter = 1;
+//Tally Light
 app.get("/tallyLight/firmware/firmware.bin", (request, response) => {
 	response.download(path.join(__dirname, "/tallyLight/firmware/firmware.bin"), "firmware.bin", (err) => {
 		if (err) {
 			console.error("Problem on download firmware: ", err);
-		} else {
-			downloadCounter++;
 		}
 	});
 	console.log("Your file has been downloaded " + downloadCounter + " times!");
@@ -34,6 +32,28 @@ app.get("/tallyLight/firmware/version.txt", (request, response) => {
 	}
 });
 
+//Datavideo
+app.get("/datavideo/firmware/firmware.bin", (request, response) => {
+	response.download(path.join(__dirname, "/datavideo/firmware/firmware.bin"), "firmware.bin", (err) => {
+		if (err) {
+			console.error("Problem on download firmware: ", err);
+		}
+	});
+});
+
+app.get("/datavideo/firmware/version", (request, response) => {
+	try {
+		const versionPath = path.join(__dirname, "/datavideo/firmware/version");
+		const versionContent = fs.readFileSync(versionPath, "utf8");
+
+		response.send(versionContent);
+		response.status(200);
+	} catch (err) {
+		console.error("Problem reading version: ", err);
+		response.status(500).send("Internal Server Error");
+	}
+});
+
 app.listen(PORT, "192.168.100.222", () => {
-	console.log("serving on: http://api.dominikkawalec.pl:3000/tallyLight/firmware/version.txt");
+	console.log("serving on: http://api.dominikkawalec.pl:3000/datavideo/firmware/version");
 });
