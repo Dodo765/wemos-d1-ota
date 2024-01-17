c3b3947c2dbfebc071c0c11c13f4826524690110
Dodo765 <104673265+Dodo765@users.noreply.github.com>
2024-01-17 21:08:18 +0100

    full pc

--- OTAServer/index.js
+++ OTAServer/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { networkInterfaces } = require("os");
 const path = require("path");
 const fs = require("fs");
+const { request } = require("http");
 
 const app = express();
 const nets = networkInterfaces();
@@ -9,19 +10,65 @@ const nets = networkInterfaces();
 // Server port
 const PORT = 3000;
 
+// //Tally Light
+// app.get("/tallyLight/firmware/firmware.bin", (request, response) => {
+// 	response.download(path.join(__dirname, "../data/tallyLight/firmware/firmware.bin"), "firmware.bin", (err) => {
+// 		if (err) {
+// 			console.error("Problem on download firmware: ", err);
+// 		}
+// 	});
+// });
+
+// app.get("/tallyLight/firmware/version.txt", (request, response) => {
+// 	try {
+// 		const versionPath = path.join(__dirname, "../data/tallyLight/firmware/version.txt");
+// 		const versionContent = fs.readFileSync(versionPath, "utf8");
+
+// 		response.send(versionContent);
+// 		response.status(200);
+// 	} catch (err) {
+// 		console.error("Problem reading version.txt: ", err);
+// 		response.status(500).send("Internal Server Error");
+// 	}
+// });
+
+// //Datavideo
+// app.get("/datavideo/firmware/firmware.bin", (request, response) => {
+// 	response.download(path.join(__dirname, "../data/datavideo/firmware/firmware.bin"), "firmware.bin", (err) => {
+// 		if (err) {
+// 			console.error("Problem on download firmware: ", err);
+// 		}
+// 	});
+// });
+
+// app.get("/datavideo/firmware/version", (request, response) => {
+// 	try {
+// 		const versionPath = path.join(__dirname, "../data/datavideo/firmware/version");
+// 		const versionContent = fs.readFileSync(versionPath, "utf8");
+
+// 		response.send(versionContent);
+// 		response.status(200);
+// 	} catch (err) {
+// 		console.error("Problem reading version: ", err);
+// 		response.status(500).send("Internal Server Error");
+// 	}
+// });
 //Tally Light
+app.get("/", (request, response) => {
+	response.send("TEST");
+	response.status(200);
+});
 app.get("/tallyLight/firmware/firmware.bin", (request, response) => {
-	response.download(path.join(__dirname, "../data/tallyLight/firmware/firmware.bin"), "firmware.bin", (err) => {
+	response.download(path.join(__dirname, "/tallyLight/firmware/firmware.bin"), "firmware.bin", (err) => {
 		if (err) {
 			console.error("Problem on download firmware: ", err);
 		}
 	});
-	console.log("Your file has been downloaded " + downloadCounter + " times!");
 });
 
-app.get("/tallyLight/firmware/version.txt", (request, response) => {
+app.get("/tallyLight/firmware/version", (request, response) => {
 	try {
-		const versionPath = path.join(__dirname, "../data/tallyLight/firmware/version.txt");
+		const versionPath = path.join(__dirname, "/tallyLight/firmware/version.txt");
 		const versionContent = fs.readFileSync(versionPath, "utf8");
 
 		response.send(versionContent);
@@ -34,7 +81,7 @@ app.get("/tallyLight/firmware/version.txt", (request, response) => {
 
 //Datavideo
 app.get("/datavideo/firmware/firmware.bin", (request, response) => {
-	response.download(path.join(__dirname, "../data/datavideo/firmware/firmware.bin"), "firmware.bin", (err) => {
+	response.download(path.join(__dirname, "/datavideo/firmware/firmware.bin"), "firmware.bin", (err) => {
 		if (err) {
 			console.error("Problem on download firmware: ", err);
 		}
@@ -43,7 +90,7 @@ app.get("/datavideo/firmware/firmware.bin", (request, response) => {
 
 app.get("/datavideo/firmware/version", (request, response) => {
 	try {
-		const versionPath = path.join(__dirname, "../data/datavideo/firmware/version");
+		const versionPath = path.join(__dirname, "/datavideo/firmware/version");
 		const versionContent = fs.readFileSync(versionPath, "utf8");
 
 		response.send(versionContent);
@@ -55,5 +102,5 @@ app.get("/datavideo/firmware/version", (request, response) => {
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-	console.log("serving on: http://api.dominikkawalec.pl:3000/datavideo/firmware/version");
+	console.log("serving on: http://pc.dominikkawalec.pl:3000/datavideo/firmware/version");
 });
