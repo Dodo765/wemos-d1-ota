bc14e87376bd86253a4aef1d8d01ed65afd3aac9
Dominik Kawalec <dkawalec76@gmail.com>
2024-05-05 22:53:37 +0200

    1.03 tally

--- OTAServer/index.js
+++ OTAServer/index.js
@@ -3,7 +3,6 @@ const { networkInterfaces } = require("os");
 const path = require("path");
 const fs = require("fs");
 const { request } = require("http");
-
 const app = express();
 const nets = networkInterfaces();
 
@@ -54,11 +53,29 @@ const PORT = 3000;
 // 	}
 // });
 //Tally Light
+function log(message) {
+	// Pobierz aktualną datę i czas
+	const now = new Date();
+	// Pobierz poszczególne elementy daty i czasu
+	const year = now.getFullYear();
+	const month = String(now.getMonth() + 1).padStart(2, "0");
+	const day = String(now.getDate()).padStart(2, "0");
+	const hour = String(now.getHours()).padStart(2, "0");
+	const minute = String(now.getMinutes()).padStart(2, "0");
+	const second = String(now.getSeconds()).padStart(2, "0");
+	// Utwórz pełny format wiadomości z datą i czasem
+	const formattedMessage = `[${year}-${month}-${day} ${hour}:${minute}:${second}] ${message}`;
+	// Wyświetl sformatowaną wiadomość w konsoli
+	console.log(formattedMessage);
+}
+
 app.get("/", (request, response) => {
+	log(request.url);
 	response.send("TEST");
 	response.status(200);
 });
 app.get("/tallyLight/firmware/firmware.bin", (request, response) => {
+	log(request.url);
 	response.download(path.join(__dirname, "/tallyLight/firmware/firmware.bin"), "firmware.bin", (err) => {
 		if (err) {
 			console.error("Problem on download firmware: ", err);
@@ -67,6 +84,7 @@ app.get("/tallyLight/firmware/firmware.bin", (request, response) => {
 });
 
 app.get("/tallyLight/firmware/version", (request, response) => {
+	log(request.url);
 	try {
 		const versionPath = path.join(__dirname, "/tallyLight/firmware/version.txt");
 		const versionContent = fs.readFileSync(versionPath, "utf8");
@@ -81,6 +99,7 @@ app.get("/tallyLight/firmware/version", (request, response) => {
 
 //Datavideo
 app.get("/datavideo/firmware/firmware.bin", (request, response) => {
+	log(request.url);
 	response.download(path.join(__dirname, "/datavideo/firmware/firmware.bin"), "firmware.bin", (err) => {
 		if (err) {
 			console.error("Problem on download firmware: ", err);
@@ -89,6 +108,7 @@ app.get("/datavideo/firmware/firmware.bin", (request, response) => {
 });
 
 app.get("/datavideo/firmware/version", (request, response) => {
+	log(request.url);
 	try {
 		const versionPath = path.join(__dirname, "/datavideo/firmware/version");
 		const versionContent = fs.readFileSync(versionPath, "utf8");
@@ -101,6 +121,13 @@ app.get("/datavideo/firmware/version", (request, response) => {
 	}
 });
 
+app.all("*", (req, res) => {
+	log(req.url);
+	res.status(404);
+	res.json({ status: "Not Found" });
+	res.end();
+});
+
 app.listen(PORT, "0.0.0.0", () => {
-	console.log("serving on: http://pc.dominikkawalec.pl:3000/datavideo/firmware/version");
+	console.log("serving on: http://api.dominikkawalec.pl:3000/tallyLight/firmware/version");
 });
